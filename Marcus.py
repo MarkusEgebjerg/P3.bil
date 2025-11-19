e0cf3fe5624a292416c6dfb6b1a988bd70b4ee54
Bjarke H. Hansen <RH57XU@student.aau.dk>
2025-11-19 10:28:41 +0100

    Chat code

--- Marcus.py
+++ Marcus.py
@@ -5,57 +5,51 @@ import pyrealsense2 as rs
 import math
 
 
-class PerceptionModule:
+class Perception_Module:
     def __init__(self):
-    # ---- Camera & RealSense setup ----
         self.pipe = rs.pipeline()
         self.cfg = rs.config()
+
         self.res_x = 1280
         self.res_y = 720
         self.fps = 30
-        self.fov_x = 87
 
         self.cfg.enable_stream(rs.stream.color, self.res_x, self.res_y, rs.format.bgr8, self.fps)
         self.cfg.enable_stream(rs.stream.depth, self.res_x, self.res_y, rs.format.z16, self.fps)
+        self.align = rs.align(rs.stream.color)
 
         self.stream = self.pipe.start(self.cfg)
-        depth_sensor = self.stream.get_device().first_depth_sensor()
-        self.depth_scale = depth_sensor.get_depth_scale()
+        self.depth_sensor = self.stream.get_device().first_depth_sensor()
+        self.depth_scale = self.depth_sensor.get_depth_scale()
+        self.depth_intrin = None
 
-        # ---- Color thresholds & morphology ----
         self.lowerYellow = np.array([20, 110, 90])
         self.upperYellow = np.array([33, 255, 255])
-
         self.lowerBlue = np.array([105, 120, 60])
         self.upperBlue = np.array([135, 255, 255])
 
         self.kernel = np.ones((2, 3))
         self.afvig = 25
 
-        self.PxD = self.res_x / self.fov_x
-        self.align = rs.align(rs.stream.color)
-
-        self.depth_intrin = None
-
         self.spatial = rs.spatial_filter()
-        self.temporal = rs.temporal_filter()
 
-    # ---------- Small helper methods ----------
+    # ------------ Perception helpers ------------
 
-    def get_frames(self):
+    def get_frame(self):
         frameset = self.pipe.wait_for_frames()
         frames = self.align.process(frameset)
+
         depth_frame = frames.get_depth_frame()
         color_frame = frames.get_color_frame()
-        if not depth_frame or not color_frame:
-            return None, None
+
         return depth_frame, color_frame
 
     def update_intrinsics(self, depth_frame):
         if self.depth_intrin is None:
             self.depth_intrin = depth_frame.profile.as_video_stream_profile().get_intrinsics()
+        return self.depth_intrin
 
-    def filter_depth(self, depth_frame):
+    def spatial_filter(self, depth_frame):
         depth_frame = self.spatial.process(depth_frame)
 
         self.spatial.set_option(rs.option.filter_magnitude, 5)
@@ -67,39 +61,34 @@ class PerceptionModule:
         depth_frame = self.spatial.process(depth_frame)
         depth_frame = depth_frame.as_depth_frame()
 
-        depth_array = np.asanyarray(depth_frame.get_data())
-        depth_img = cv2.applyColorMap(
-            cv2.convertScaleAbs(depth_array, alpha=0.06),
-            cv2.COLORMAP_JET
-        )
-        return depth_frame, depth_img
+        return depth_frame
 
-    def preprocess_color(self, color_frame):
+    def color_space_conversion(self, color_frame):
         color_array = np.asanyarray(color_frame.get_data())
         frame_HSV = cv2.cvtColor(color_array, cv2.COLOR_BGR2HSV)
-        return color_array, frame_HSV
+        return frame_HSV, color_array
 
-    def masking(self, mask):
-        mask_open = cv2.erode(mask, self.kernel, iterations=3)
-        mask_close = cv2.dilate(mask_open, self.kernel, iterations=10)
-        return mask_close
+    def mask_clean(self, mask):
+        mask_Open = cv2.erode(mask, self.kernel, iterations=3)
+        mask_Close = cv2.dilate(mask_Open, self.kernel, iterations=10)
+        return mask_Close
 
-    def threshold_and_mask(self, frame_HSV):
+    def color_detector(self, frame_HSV):
         frame_threshold_Y = cv2.inRange(frame_HSV, self.lowerYellow, self.upperYellow)
         frame_threshold_B = cv2.inRange(frame_HSV, self.lowerBlue, self.upperBlue)
 
-        mask_y = self.masking(frame_threshold_Y)
-        mask_b = self.masking(frame_threshold_B)
-        return mask_y, mask_b
+        clean_mask_y = self.mask_clean(frame_threshold_Y)
+        clean_mask_b = self.mask_clean(frame_threshold_B)
+        return clean_mask_y, clean_mask_b
 
-    def find_contours(self, mask_y, mask_b):
-        contours_b, _ = cv2.findContours(mask_b, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        contours_y, _ = cv2.findContours(mask_y, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        return contours_b, contours_y
+    def contour_finder(self, clean_mask_y, clean_mask_b):
+        contours_b, _ = cv2.findContours(clean_mask_b, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours_y, _ = cv2.findContours(clean_mask_y, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        return contours_y, contours_b
 
-    def find_contour_centers(self, contours_b, contours_y, color_array):
+    def contour_center_finder(self, contours_y, contours_b, color_array):
+        # fresh per call
         contour_centers = []
-
         for i in range(2):
             lst = contours_b
             if i == 1:
@@ -130,136 +119,185 @@ class PerceptionModule:
 
         return contour_centers
 
-    def select_cones(self, contour_centers, color_array):
+    def contour_control(self, contour_centers, color_array):
         cone_positions = []
-
         for i in range(0, len(contour_centers)):
             p = 0
             for j in range(0, len(contour_centers)):
 
                 if (
-                    i != j and
-                    -self.afvig < (contour_centers[i][0] - contour_centers[j][0]) < self.afvig and
-                    contour_centers[i][1] < contour_centers[j][1]
+                    i != j
+                    and -self.afvig < (contour_centers[i][0] - contour_centers[j][0]) < self.afvig
+                    and contour_centers[i][1] < contour_centers[j][1]
                 ):
                     cone_positions.append(contour_centers[i])
 
                 if (
-                    i != j and
-                    (-self.afvig > (contour_centers[i][0] - contour_centers[j][0]) or
-                     self.afvig < (contour_centers[i][0] - contour_centers[j][0]))
+                    i != j
+                    and (
+                        -self.afvig > (contour_centers[i][0] - contour_centers[j][0])
+                        or self.afvig < (contour_centers[i][0] - contour_centers[j][0])
+                    )
                 ):
-                    p += 1
+                    p = p + 1
                     if p >= len(contour_centers) - 1:
-                        cv2.circle(
-                            color_array,
-                            (contour_centers[i][0], contour_centers[i][1]),
-                            4, (255, 255, 255), -1
-                        )
                         cone_positions.append(contour_centers[i])
+        return cone_positions, color_array
 
-        return cone_positions
-
-    def project_cones(self, cone_positions, depth_frame, color_array):
+    def world_positioning(self, cone_positions, depth_frame, depth_intrin, color_array):
+        """
+        Convert image-space cone centers to world coordinates.
+        Returns:
+            color_array_with_text, world_cones
+        where world_cones is a list of (u, v, X, Y, Z, color)
+        """
         world_cones = []
 
-        for cx, cy, ccolor in cone_positions:
-            u = float(cx)
-            v = float(cy)
-
+        for i in range(0, len(cone_positions)):
+            u = float(cone_positions[i][0])  # pixel x
+            v = float(cone_positions[i][1])  # pixel y
             depth_m = float(depth_frame.get_distance(int(u), int(v)))
             if depth_m <= 0:
                 continue
 
-            X, Y, Z = rs.rs2_deproject_pixel_to_point(self.depth_intrin, [u, v], depth_m)
+            X, Y, Z = rs.rs2_deproject_pixel_to_point(depth_intrin, [u, v], depth_m)
             X = round(X, 2)
             Y = round(Y, 2)
             Z = round(Z, 2)
+            color = cone_positions[i][2]
 
-            world_cones.append((X, Y, Z, ccolor))
+            world_cones.append((u, v, X, Y, Z, color))
 
+            cv2.circle(color_array, (int(u), int(v)), 4, (255, 255, 255), -1)
             cv2.putText(
                 color_array,
-                f" c: {ccolor} coo: [{X}, {Z}]",
-                (int(u) - 250, int(v)),
+                f" c:{color} coo:[{X}, {Z}]",
+                (int(u), int(v)),
                 cv2.FONT_HERSHEY_DUPLEX,
                 0.5,
                 (0, 0, 255)
             )
 
-        return world_cones, color_array
+        return color_array, world_cones
 
-    # ---------- One perception step ----------
+    # ------------ Perception main step ------------
 
-    def step(self, logic_module, motor_module):
-        depth_frame, color_frame = self.get_frames()
+    def run(self, logic_module):
+        depth_frame, color_frame = self.get_frame()
         if depth_frame is None:
-            return True  # continue loop
+            return True
 
-        self.update_intrinsics(depth_frame)
+        depth_intrin = self.update_intrinsics(depth_frame)
+        depth_frame = self.spatial_filter(depth_frame)
+        frame_HSV, color_array = self.color_space_conversion(color_frame)
 
-        depth_frame, depth_img = self.filter_depth(depth_frame)
-        color_array, frame_HSV = self.preprocess_color(color_frame)
-        mask_y, mask_b = self.threshold_and_mask(frame_HSV)
-        contours_b, contours_y = self.find_contours(mask_y, mask_b)
-        contour_centers = self.find_contour_centers(contours_b, contours_y, color_array)
-        cone_positions = self.select_cones(contour_centers, color_array)
-        world_cones, color_array = self.project_cones(cone_positions, depth_frame, color_array)
+        # ---- CROP TO BOTTOM HALF ----
+        h, w = frame_HSV.shape[:2]
+        y_offset = h // 2  # start row of bottom half
 
-        # Later:
-        # decisions = logic_module.update(world_cones)
-        # motor_module.update(decisions)
+        frame_HSV_crop = frame_HSV[y_offset:h, 0:w]
+        color_crop = color_array[y_offset:h, 0:w]
 
-        cv.imshow("thresh", color_array)
-        cv.imshow("depth", depth_img)
+        # Detection only on cropped region
+        clean_mask_y, clean_mask_b = self.color_detector(frame_HSV_crop)
+        contours_y, contours_b = self.contour_finder(clean_mask_y, clean_mask_b)
+        contour_centers_crop = self.contour_center_finder(contours_y, contours_b, color_crop)
 
+        # Shift contour centers back to full-image coordinates
+        contour_centers = [
+            (cx, cy + y_offset, col) for (cx, cy, col) in contour_centers_crop
+        ]
+
+        cone_positions, _ = self.contour_control(contour_centers, color_array)
+        world_img, world_cones = self.world_positioning(cone_positions, depth_frame, depth_intrin, color_array)
+
+        # Logic module: pair cones, compute midpoints, draw them
+        output_img, midpoints_world = logic_module.update(world_cones, world_img)
+
+        cv.imshow("thresh", output_img)
         if cv.waitKey(1) & 0xFF == ord('q'):
-            return False  # stop loop
+            return False
 
-        return True  # keep running
+        return True
 
     def shutdown(self):
         self.pipe.stop()
         cv.destroyAllWindows()
 
 
-class LogicModule:
-    def __init__(self):
-        pass
+class Logic_Module:
+    """
+    Logic layer:
+    - pairs closest blue & yellow cones
+    - finds midpoints between pairs
+    - (later) will feed motor control
+    """
 
-    def update(self, world_cones):
-        # TODO: implement path planning / decisions
-        return None
+    def update(self, world_cones, img):
+        """
+        world_cones: list of (u, v, X, Y, Z, color)
+        img: current frame to draw on
+        """
+        img, midpoints_world = self.pair_and_draw_midpoints(world_cones, img)
+        # later you can return midpoints_world to motor control
+        return img, midpoints_world
 
+    def pair_and_draw_midpoints(self, world_cones, img):
+        # Separate cones by color
+        blue_cones = [c for c in world_cones if c[5] == "Blue"]
+        yellow_cones = [c for c in world_cones if c[5] == "Yellow"]
 
-class MotorControlModule:
-    def __init__(self):
-        pass
+        # Sort each list by forward distance Z (index 4)
+        blue_cones.sort(key=lambda c: c[4])
+        yellow_cones.sort(key=lambda c: c[4])
 
-    def update(self, command):
-        # TODO: send commands to motors/servo
-        pass
+        n_pairs = min(len(blue_cones), len(yellow_cones))
+        midpoints_world = []  # (midX, midZ)
+
+        for i in range(n_pairs):
+            ub, vb, Xb, Yb, Zb, cb = blue_cones[i]
+            uy, vy, Xy, Yy, Zy, cy = yellow_cones[i]
+
+            # Midpoint in world coordinates (X/Z plane)
+            midX = (Xb + Xy) / 2.0
+            midZ = (Zb + Zy) / 2.0
+            midpoints_world.append((midX, midZ))
+
+            # Midpoint in image space for drawing
+            mid_u = (ub + uy) / 2.0
+            mid_v = (vb + vy) / 2.0
+
+            cv2.circle(img, (int(mid_u), int(mid_v)), 6, (0, 255, 0), -1)
+            cv2.putText(
+                img,
+                "MID",
+                (int(mid_u) + 5, int(mid_v)),
+                cv2.FONT_HERSHEY_DUPLEX,
+                0.5,
+                (0, 255, 0),
+                1
+            )
+
+        return img, midpoints_world
 
 
 def main():
     try:
-        perception = PerceptionModule()
+        perception = Perception_Module()
     except RuntimeError as e:
-        print(f"[ERROR] {e}")
-        print("-> Check that your RealSense is plugged in and not used by another program.")
+        print(e)
         return
 
-    logic = LogicModule()
-    motor = MotorControlModule()
+    logic = Logic_Module()
 
     try:
         while True:
-            keep_running = perception.step(logic, motor)
+            keep_running = perception.run(logic)
             if not keep_running:
                 break
     finally:
         perception.shutdown()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
